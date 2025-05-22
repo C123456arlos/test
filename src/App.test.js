c555d5365bf8db76e6786e0e62a4e58bc8bf0bf4
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2025-05-22 16:38:38 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,11 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText.getByTestId('learn-link');
   expect(linkElement).toBeInTheDocument();
 });
+test('url is correct', () => {
+  render(<App></App>)
+  const linkElement = screen.getByText.getByTestId('learn-link')
+  expect(linkElement.href).toContain('https://www.google.com')
+})
